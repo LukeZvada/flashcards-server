82a0fb3f3776a9f747eff651a2029d54db4f8391
Chris Johnson <christopherjohnson1@gmail.com>
2021-02-04 10:34:01 -0600

    questions fixtures added to bash script

--- reset.sh
+++ reset.sh
@@ -5,3 +5,11 @@ python manage.py makemigrations flashcardsapi
 python manage.py migrate
 python manage.py loaddata users
 python manage.py loaddata tokens
+python manage.py loaddata questionsBE1a
+python manage.py loaddata questionsBE1b
+python manage.py loaddata questionsBE2
+python manage.py loaddata questionsBE3
+python manage.py loaddata questionsFE1
+python manage.py loaddata questionsFE2
+python manage.py loaddata questionsFE3
+python manage.py loaddata questionsFE4
